65a530deaabe5b174b8bc207b277b454adffba66
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2023-12-06 01:15:27 +0600

    s

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        String str;
+        System.out.println(str = (first > second) ? "first" : "second");
     }
 }
